3379d526f9305a9c2b35d91e74bdb423438b8250
Mike Tegtmeyer <tegtmeye@gmail.com>
2018-02-24 21:31:57 -0500

    Made building the examples a configure option [default=yes]. Made example simpler

--- examples/impatient.cc
+++ examples/impatient.cc
@@ -11,8 +11,8 @@ int main (int argc, char *argv[])
     // accepts --help and -h without an argument
     co::make_option("help,h","Display a help message"),
 
-    // accepts -b <arg> The mapped value is a string
-    co::make_option(",b",co::value<std::string>(),
+    // accepts --foo <arg> or -f <arg> The mapped value is a string
+    co::make_option("foo,f",co::value<std::string>(),
       "Description of option that requires a string argument"),
 
     // accepts -i <arg> or -i The mapped value is an int with default 42
@@ -33,32 +33,21 @@ int main (int argc, char *argv[])
     return 0;
   }
 
-  auto brange = vm.equal_range("b");
-  if(brange.first != brange.second)
-    std::cout << "-b has given values:\n";
-  else
-    std::cout << "-b was not provided\n";
+  auto brange = vm.equal_range("foo");
   for (auto i = brange.first; i != brange.second; ++i) {
-    std::cout << "  " << i->first << ": "
+    std::cout << i->first << " was given: "
       << co::any_cast<std::string>(i->second) << '\n';
   }
 
   auto irange = vm.equal_range("i");
-  if(irange.first != irange.second)
-    std::cout << "-i has given values:\n";
-  else
-    std::cout << "-i was not provided\n";
-  for (auto i = irange.first; i != irange.second; ++i)
-    std::cout << "  " << i->first << ": "
+  for (auto i = irange.first; i != irange.second; ++i) {
+    std::cout << i->first << " was given: "
       << co::any_cast<int>(i->second) << '\n';
+  }
 
-  auto orange = vm.equal_range("");
-  if(orange.first != orange.second)
-    std::cout << "Operands have values:\n";
-  else
-    std::cout << "An operand was not provided\n";
-  for (auto i = orange.first; i != orange.second; ++i)
-    std::cout << "  " << i->first << ": "
+  auto oprange = vm.equal_range("");
+  for (auto i = oprange.first; i != oprange.second; ++i)
+    std::cout << "operand was given with value: "
       << co::any_cast<std::string>(i->second) << '\n';
 
   return 0;
