32545593f3400cdb3ca7555b1fe36961af4e0614
Michael B. Tegtmeyer <tegtmeye@gmail.com>
2020-04-09 16:32:43 -0400

    Updated impatient example for additional preempt feature

--- examples/impatient.cc
+++ examples/impatient.cc
@@ -30,52 +30,177 @@
 #include <string>
 #include <iostream>
 
+namespace co = cmd_options;
+
 int main (int argc, char *argv[])
 {
-  namespace co = cmd_options;
+  try {
+    co::options_group grp{
+      /*
+        Accepts --help and -h without an argument. If seen, stop further
+        options parsing
+      */
+      co::make_option("help,h","Display a help message",
+        co::constrain().preempt()),
 
-  co::options_group grp{
-    // accepts --help and -h without an argument
-    co::make_option("help,h","Display a help message"),
+      /*
+        Accepts --foo <arg> or -f <arg> with a required string argument.
+        This option can be given any number of times.
+      */
+      co::make_option("foo,f",co::value<std::string>(),
+        "Description of option that requires a string argument"),
 
-    // accepts --foo <arg> or -f <arg> The mapped value is a string
-    co::make_option("foo,f",co::value<std::string>(),
-      "Description of option that requires a string argument"),
+      /*
+        Accepts --bar or -b strictly without and argument
+        This option can be only be given 0 or 1 time
+      */
+      co::make_option("bar,b","Description of option without an argument",
+        co::constrain().occurrences(0,1)),
 
-    // accepts -i <arg> or -i The mapped value is an int with default 42
-    // indexed by the 'meaning_of_life' key
-    co::make_option(",i,meaning_of_life",co::value<int>().implicit(42),
-      "Description of option with an optional integer argument"),
+      /*
+        Accepts -i <arg> or -i with an optional int argument indexed in
+        the resulting multimap by the 'meaning_of_life' key. If the
+        argument is not provided, the mapped value will default 42.
+        This option can be given zero or one time.
+      */
+      co::make_option(",i,meaning_of_life",co::value<int>().implicit(42),
+        "Description of option with an optional integer argument",
+        co::constrain().occurrences(0,1)),
 
-    // accepts an operand in any position. The mapped value is a string
-    co::make_operand("operand_key",co::value<std::string>())
-  };
+      /*
+        Specify a non-optional operand at index position 0 only. The
+        mapped value is a string.
+      */
+      co::make_operand("operand1",co::value<std::string>(),
+        co::constrain().at_position(0).occurrences(1)),
 
-  // parse the options and put into the variable map. Don't forget that
-  // the first argument (argv[0]) is the program name!
-  co::variable_map vm = co::parse_arguments(argv+1,argv+argc,grp);
+      /*
+        Specify an optional operand at index position 1 only. The
+        mapped value is a string. If this operand is given, '-i' cannot
+        also be given.
+      */
+      co::make_operand("operand2",co::value<std::string>(),
+        co::constrain().at_position(1).mutually_exclusive({"meaning_of_life"})),
 
-  if(vm.count("help")) {
-    std::cout << co::to_string(grp) << "\n";
-    return 0;
+      /*
+        Accepts an optional operand in any position but since it is
+        assigned to the options group after 'operand1' and 'operand2',
+        it is only considered in the position index 2 and greater. The
+        mapped value is a string. This operand can be given any number
+        of times if it is, '--bar' must also be given.
+      */
+      co::make_operand("operand3",co::value<std::string>(),
+        co::constrain().mutually_inclusive({"bar"})),
+    };
+
+    // parse the options and put into the variable map. Don't forget that
+    // the first argument (argv[0]) is the program name!
+    co::variable_map vm = co::parse_arguments(argv+1,argv+argc,grp);
+
+    if(vm.count("help")) {
+      std::cout << co::to_string(grp) << "\n";
+      return 0;
+    }
+
+    auto range = vm.equal_range("foo");
+    for (auto i = range.first; i != range.second; ++i) {
+      std::cout << i->first << " was given: "
+        << co::any_cast<std::string>(i->second) << '\n';
+    }
+
+    range = vm.equal_range("meaning_of_life");
+    for (auto i = range.first; i != range.second; ++i) {
+      std::cout << i->first << " was given: "
+        << co::any_cast<int>(i->second) << '\n';
+    }
+
+    range = vm.equal_range("operand1");
+    for (auto i = range.first; i != range.second; ++i)
+      std::cout << i->first << " was given: "
+        << co::any_cast<std::string>(i->second) << '\n';
+
+    range = vm.equal_range("operand2");
+    for (auto i = range.first; i != range.second; ++i)
+      std::cout << i->first << " was given: "
+        << co::any_cast<std::string>(i->second) << '\n';
+
+    range = vm.equal_range("operand3");
+    for (auto i = range.first; i != range.second; ++i)
+      std::cout << i->first << " was given: "
+        << co::any_cast<std::string>(i->second) << '\n';
   }
-
-  auto brange = vm.equal_range("foo");
-  for (auto i = brange.first; i != brange.second; ++i) {
-    std::cout << i->first << " was given: "
-      << co::any_cast<std::string>(i->second) << '\n';
+  catch(const co::unknown_option_error &ex) {
+    std::cerr << "unrecognized option '" << argv[ex.argument()+1] << "'\n"
+      << "Try 'impatient --help' for more information.\n";
   }
-
-  auto irange = vm.equal_range("meaning_of_life");
-  for (auto i = irange.first; i != irange.second; ++i) {
-    std::cout << i->first << " was given: "
-      << co::any_cast<int>(i->second) << '\n';
+  catch(const co::missing_argument_error &ex) {
+    std::cerr << "option '" << argv[ex.argument()+1]
+      << "' requires an argument\n"
+      << "Try 'impatient --help' for more information.\n";
   }
+  catch(const co::unexpected_argument_error &ex) {
+    std::cerr << "option '" << argv[ex.argument()+1]
+      << "' doesn't allow an argument\n"
+      << "Try 'impatient --help' for more information.\n";
+  }
+  catch(const co::invalid_argument_error &ex) {
+    std::string nested_msg;
+    try {
+      std::rethrow_if_nested(ex);
+    }
+    catch(const std::exception& nested) {
+      nested_msg = nested.what();
+    }
+    catch(...) {
+      nested_msg = "unknown error";
+    }
 
-  auto oprange = vm.equal_range("operand_key");
-  for (auto i = oprange.first; i != oprange.second; ++i)
-    std::cout << i->first << " was given: "
-      << co::any_cast<std::string>(i->second) << '\n';
+    std::cerr << "invalid argument to '" << argv[ex.argument()+1] << "', "
+      << nested_msg.c_str() << "\n"
+      << "Try 'impatient --help' for more information.\n";
+  }
+  catch(const co::unexpected_operand_error &ex) {
+    std::cerr << "unexpected operand '" << argv[ex.argument()+1] << "'\n"
+      << "Try 'impatient --help' for more information.\n";
+  }
+  catch(const co::occurrence_error &ex) {
+    if(ex.min() == ex.max()) {
+      std::cerr << ex.mapped_key()
+        << " must be provided exactly " << ex.max() << " time"
+        << (ex.max() == 1?"":"s") << ", received "
+        << ex.occurrences() << "\n";
+    }
+    else if(ex.min() == 0) {
+      std::cerr << ex.mapped_key()
+        << " cannot be provided more than " << ex.max() << " time"
+        << (ex.max() == 1?"":"s") << ", received "
+        << ex.occurrences() << "\n";
+    }
+    else if(ex.max() == std::numeric_limits<std::size_t>::max()) {
+      std::cerr << ex.mapped_key()
+        << " must be provided a minimum of " << ex.min() << " time"
+        << (ex.min() == 1?"":"s") << ", received "
+        << ex.occurrences() << "\n";
+    }
+    else {
+      std::cerr << ex.mapped_key()
+        << " must be provided a minimum of " << ex.min() << " but no more than "
+        << ex.max() << " time" << (ex.max() == 1?"":"s") << ", received "
+        << ex.occurrences() << "\n";
+    }
+
+    std::cerr << "Try 'impatient --help' for more information.\n";
+  }
+  catch(const co::mutually_exclusive_error &ex) {
+    std::cerr << ex.mapped_key() << " cannot be used with "
+      << ex.exclusive_mapped_key() << "\n"
+      << "Try 'impatient --help' for more information.\n";
+  }
+  catch(const co::mutually_inclusive_error &ex) {
+    std::cerr << ex.mapped_key() << " must be used with "
+      << ex.inclusive_mapped_key() << "\n"
+      << "Try 'impatient --help' for more information.\n";
+  }
 
   return 0;
 }
